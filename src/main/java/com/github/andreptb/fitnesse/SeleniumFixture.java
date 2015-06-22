038d0f0b7530f7dd28baecb2f3d4ce358a4a29fa
Andre Albino Pereira <andre@softplan.com.br>
2015-06-21 22:48:51 -0300

    added support for special keys, added wait for element presence mechanism, first try to make travis build work with custom fitnesse dep

--- src/main/java/com/github/andreptb/fitnesse/SeleniumFixture.java
+++ src/main/java/com/github/andreptb/fitnesse/SeleniumFixture.java
@@ -3,14 +3,17 @@ package com.github.andreptb.fitnesse;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.EnumUtils;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.github.andreptb.fitnesse.util.FitnesseMarkup;
-import com.github.andreptb.fitnesse.util.SeleniumElementFinder;
 import com.github.andreptb.fitnesse.util.FixtureWebDriverProvider;
+import com.github.andreptb.fitnesse.util.SeleniumElementFinder;
 
 /**
  * Slim fixture to execute Selenium commands, see README.md for more information.
@@ -28,10 +31,6 @@ public class SeleniumFixture {
 	}
 
 	/**
-	 * HTTP scheme prefix, to detect remote DRIVER
-	 */
-	private static final String HTTP_PREFIX = "http://";
-	/**
 	 * HTML Value attribute, usually used on inputs
 	 */
 	private static final String INPUT_VALUE_ATTRIBUTE = "value";
@@ -44,6 +43,10 @@ public class SeleniumFixture {
 	 * Selenium Web Driver, static so the same DRIVER instance can be used through multiple tables
 	 */
 	private static WebDriver DRIVER;
+	/**
+	 * Timeout time to wait for elements to be present. Default is 60 seconds
+	 */
+	private static int WAIT_TIMEOUT = 60;
 	/**
 	 * Utility to help creating WebDriver instances
 	 */
@@ -97,12 +100,27 @@ public class SeleniumFixture {
 	}
 
 	/**
+	 * Sets the time to wait for a element or text presence.
+	 * Influences the following methods:
+	 * <ul>
+	 * <li>{@link #present(String)}</li>
+	 * </ul>
+	 * <code>
+	 * | set wait timeout | <i>timeout in seconds</i> |
+	 * </code>
+	 */
+	public boolean setWaitTimeout(int timeoutInSeconds) {
+		SeleniumFixture.WAIT_TIMEOUT = timeoutInSeconds;
+		return true;
+	}
+
+	/**
 	 * Returns if browser is available and can be used
 	 *
-	 * | ensure | available |
+	 * | ensure | browser available |
 	 * @return browserStarted
 	 */
-	public boolean available() {
+	public boolean browserAvailable() {
 		return SeleniumFixture.BROWSER_STATE != BrowserState.UNITIALIZED;
 	}
 	/**
@@ -115,7 +133,7 @@ public class SeleniumFixture {
 	 * @return result Boolean result indication of assertion/operation
 	 */
 	public boolean open(String url) {
-		if(available()) {
+		if(browserAvailable()) {
 			SeleniumFixture.DRIVER.get(this.fitnesseMarkup.clean(url));
 			SeleniumFixture.BROWSER_STATE = BrowserState.NAVIGATING;
 			return true;
@@ -132,7 +150,7 @@ public class SeleniumFixture {
 	 * @return result Boolean result indication of assertion/operation
 	 */
 	public String title() {
-		if(available()) {
+		if(browserAvailable()) {
 			return SeleniumFixture.DRIVER.getTitle();
 		}
 		return null;
@@ -147,7 +165,7 @@ public class SeleniumFixture {
 	 * @return result Boolean result indication of assertion/operation
 	 */
 	public boolean close() {
-		if(available() && SeleniumFixture.BROWSER_STATE != BrowserState.CLOSED) {
+		if(browserAvailable() && SeleniumFixture.BROWSER_STATE != BrowserState.CLOSED) {
 			SeleniumFixture.DRIVER.close();
 			SeleniumFixture.BROWSER_STATE = BrowserState.CLOSED;
 			return true;
@@ -158,34 +176,47 @@ public class SeleniumFixture {
 	/**
 	 * Sets the value of an input field, as though you typed it in.
 	 * Can also be used to set the value of combo boxes, check boxes, etc. In these cases, value should be the value of the option selected, not the visible text.
+	 * Last but no least, if the value matches any special key from {@link Keys}, the special key will be typed without clearing the element. Useful when you need
+	 * to type something and then press enter or tab.
+	 * <p>
+	 * <code>
+	 * | type | <i>value</i> | in | <i>locator</i> |
+	 * </code>
+	 * </p>
 	 *
-	 * <p><code>
-	 * | type | value | in | locator |
-	 * </code></p>
 	 * @param value the value to typeIn
 	 * @param locator an element locator
 	 * @return result Boolean result indication of assertion/operation
 	 */
 	public boolean typeIn(String value, String locator) {
-		if(available()) {
-			WebElement element = this.elementFinder.find(SeleniumFixture.DRIVER, locator);
-			element.clear();
-			element.sendKeys(value);
+		if (!browserAvailable()) {
+			return false;
+		}
+		WebElement element = this.elementFinder.find(SeleniumFixture.DRIVER, locator);
+		String cleanedValue = this.fitnesseMarkup.clean(value);
+		Keys specialKey = EnumUtils.getEnum(Keys.class, StringUtils.upperCase(cleanedValue));
+		if (specialKey != null) {
+			element.sendKeys(specialKey);
 			return true;
 		}
-		return false;
+		element.clear();
+		element.sendKeys(cleanedValue);
+		return true;
 	}
 
 	/**
 	 * Clicks on a link, button, checkbox or radio button. If the click action causes a new page to load (like a link usually does), call waitForPageToLoad.
-	 * <p><code>
-	 * | click | locator |
-	 * </code></p>
+	 * <p>
+	 * <code>
+	 * | click | <i>locator</i> |
+	 * </code>
+	 * </p>
+	 *
 	 * @param locator an element locator
 	 * @return result Boolean result indication of assertion/operation
 	 */
 	public boolean click(String locator) {
-		if(available()) {
+		if(browserAvailable()) {
 			this.elementFinder.find(SeleniumFixture.DRIVER, locator).click();
 			return true;
 		}
@@ -193,16 +224,19 @@ public class SeleniumFixture {
 	}
 
 	/**
-	 * Gets the (whitespace-trimmed) value of an input field (or anything else with a value parameter). For checkbox/radio elements, the value will be "on" or "off" depending on whether the element is checked or not.
+	 * Gets the (whitespace-trimmed) value of an input field (or anything else with a value parameter). For checkbox/radio elements, the value will be "on" or "off" depending on whether the element is
+	 * checked or not.
+	 * <p>
+	 * <code>
+	 * | check | <i>value</i> | <i>locator</i> | expectedValue |
+	 * </code>
+	 * </p>
 	 *
-	 * <p><code>
-	 * | check | value | locator | expectedValue |
-	 * </code></p>
 	 * @param locator an element locator
 	 * @return value associated with the locator
 	 */
 	public String value(String locator) {
-		if(available()) {
+		if(browserAvailable()) {
 			WebElement element = this.elementFinder.find(SeleniumFixture.DRIVER, locator);
 			return this.fitnesseMarkup.clean(element.getAttribute(SeleniumFixture.INPUT_VALUE_ATTRIBUTE));
 		}
@@ -210,16 +244,19 @@ public class SeleniumFixture {
 	}
 
 	/**
-	 * Gets the text of an element. This works for any element that contains text. This command uses either the textContent (Mozilla-like browsers) or the innerText (IE-like browsers) of the element, which is the rendered text shown to the user.
+	 * Gets the text of an element. This works for any element that contains text. This command uses either the textContent (Mozilla-like browsers) or the innerText (IE-like browsers) of the element,
+	 * which is the rendered text shown to the user.
+	 * <p>
+	 * <code>
+	 * | check | text | <i>locator</i> | expectedValue |
+	 * </code>
+	 * </p>
 	 *
-	 * <p><code>
-	 * | check | text | locator | expectedValue |
-	 * </code></p>
 	 * @param locator an element locator
 	 * @return text associated with the locator
 	 */
 	public String text(String locator) {
-		if(available()) {
+		if(browserAvailable()) {
 			WebElement element = this.elementFinder.find(SeleniumFixture.DRIVER, locator);
 			return this.fitnesseMarkup.clean(element.getText());
 		}
@@ -228,14 +265,16 @@ public class SeleniumFixture {
 
 	/**
 	 * Takes screenshot from current browser state and returns to be previewed in test result page.
-	 *
-	 * <p><code>
+	 * <p>
+	 * <code>
 	 * | show | screenshot |
-	 * </code></p>
-	 * @return screenshot preview and download url
+	 * </code>
+	 * </p>
+	 *
+	 * @return screenshot saved file absolute path
 	 */
 	public String screenshot() throws IOException {
-		if(!available() || BrowserState.NAVIGATING != SeleniumFixture.BROWSER_STATE) {
+		if(!browserAvailable() || BrowserState.NAVIGATING != SeleniumFixture.BROWSER_STATE) {
 			return null;
 		}
 		if(SeleniumFixture.DRIVER instanceof TakesScreenshot) {
@@ -246,16 +285,18 @@ public class SeleniumFixture {
 
 	/**
 	 * Verifies that the specified element is somewhere on the page.
+	 * <p>
+	 * <code>
+	 * | ensure | present | <i>locator</i> |
+	 * </code>
+	 * </p>
 	 *
-	 * <p><code>
-	 * | ensure | present | locator |
-	 * </code></p>
 	 * @param locator an element locator
 	 * @return result Boolean result indication of assertion/operation
 	 */
 	public boolean present(String locator) {
-		if(available()) {
-			return this.elementFinder.contains(SeleniumFixture.DRIVER, locator);
+		if(browserAvailable()) {
+			return this.elementFinder.contains(SeleniumFixture.DRIVER, locator, SeleniumFixture.WAIT_TIMEOUT);
 		}
 		return false;
 	}
--- src/main/java/com/github/andreptb/fitnesse/util/SeleniumElementFinder.java
+++ src/main/java/com/github/andreptb/fitnesse/util/SeleniumElementFinder.java
@@ -4,8 +4,11 @@ import org.apache.commons.lang3.EnumUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Utility to find elements supporting different selectors.
@@ -14,68 +17,117 @@ import org.openqa.selenium.WebElement;
  */
 public class SeleniumElementFinder {
 
-    /**
-     * Locator selector typeIn separator constant
-     */
-    private static final String SELECTOR_TYPE_SEPARATOR = "=";
-    /**
-     * Utility to process FitNesse markup so can be used by Selenium WebDriver
-     */
-    private FitnesseMarkup fitnesseMarkup = new FitnesseMarkup();
+	/**
+	 * Locator selector typeIn separator constant
+	 */
+	private static final String SELECTOR_TYPE_SEPARATOR = "=";
+	/**
+	 * Utility to process FitNesse markup so can be used by Selenium WebDriver
+	 */
+	private FitnesseMarkup fitnesseMarkup = new FitnesseMarkup();
 
-    enum SelectorType {
-        id(By.ById.class),
-        name(By.ByName.class),
-        css(By.ByCssSelector.class),
-        link(By.ByLinkText.class);
+	enum SelectorType {
+		id(By.ById.class),
+		name(By.ByName.class),
+		css(By.ByCssSelector.class),
+		link(By.ByLinkText.class);
 
-        private Class<? extends By> byClass;
+		private Class<? extends By> byClass;
 
-        SelectorType(Class<? extends By> byClass) {
-            this.byClass = byClass;
-        }
-    }
+		SelectorType(Class<? extends By> byClass) {
+			this.byClass = byClass;
+		}
+	}
 
-    /**
-     * Selects element. Tries to emulate util IDE searching methods
-     * <ul>
-     *     <li>By id: 'id=&lt;id&gt;</li>'
-     *     <li>By name: 'name=&lt;name&gt;</li>'
-     *     <li>By css selector: 'css=#&lt;id&gt;</li>'
-     *     <li>By link text selector: 'link=#&lt;linKtEXT&gt;</li>'
-     *     <li>By xpath selector: 'div[@id=&lt;id&gt;]</li>'
-     * </ul>
-     * @param locator an element locator
-     * @return webElementFound
-     * @throws NoSuchElementException if element don't exist or cannot be found
-     */
-    public WebElement find(WebDriver driver, String locator) {
-        String cleanedLocator = fitnesseMarkup.clean(locator);
-        String selectorPrefix = StringUtils.substringBefore(cleanedLocator, SeleniumElementFinder.SELECTOR_TYPE_SEPARATOR);
-        SelectorType selectorType = EnumUtils.getEnum(SelectorType.class, selectorPrefix);
-        if(selectorType == null) {
-            return driver.findElement(By.xpath(locator));
-        }
-        try {
-            String parsedLocator = StringUtils.removeStart(cleanedLocator, selectorPrefix + SeleniumElementFinder.SELECTOR_TYPE_SEPARATOR);
-            return driver.findElement(selectorType.byClass.getConstructor(String.class).newInstance(parsedLocator));
-        } catch(ReflectiveOperationException e) {
-            throw new IllegalStateException("Unexpected failure instantiating selector: " + selectorPrefix, e);
-        }
-    }
+	/**
+	 * Checks if element is contained in the page.
+	 *
+	 * @see #find(WebDriver, String)
+	 * @param driver instance of {@link WebDriver}
+	 * @param locator an element locator
+	 * @return containsElement true if found, false otherwise
+	 */
+	public boolean contains(WebDriver driver, String locator) {
+		try {
+			find(driver, locator);
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+		return true;
+	}
 
-    /**
-     * Checks if element is contained in the page.
-     * @see #find(WebDriver, String)
-     * @param locator an element locator
-     * @return containsElement true if found, false otherwise
-     */
-    public boolean contains(WebDriver driver, String locator) {
-        try {
-            find(driver, locator);
-        } catch(NoSuchElementException e) {
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * Selects element.
+	 *
+	 * @see #parse(String)
+	 * @param driver instance of {@link WebDriver}
+	 * @param locator an element locator
+	 * @return webElementFound
+	 * @throws NoSuchElementException if element don't exist or cannot be found
+	 */
+	public WebElement find(WebDriver driver, String locator) {
+		return driver.findElement(parse(locator));
+	}
+
+	/**
+	 * Checks if element is contained in the page, waiting for a certain time
+	 *
+	 * @see #find(WebDriver, String, int)
+	 * @param driver instance of {@link WebDriver}
+	 * @param locator an element locator
+	 * @param timeoutInSeconds time to wait for element
+	 * @return containsElement true if found, false otherwise
+	 */
+	public boolean contains(WebDriver driver, String locator, int timeoutInSeconds) {
+		try {
+			find(driver, locator, timeoutInSeconds);
+		} catch (TimeoutException e) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Tries to selects element, waiting until is available.
+	 *
+	 * @see #parse(String)
+	 * @param driver instance of {@link WebDriver}
+	 * @param locator an element locator
+	 * @param timeoutInSeconds time to wait for element
+	 * @return webElementFound
+	 * @throws NoSuchElementException if element don't exist or cannot be found
+	 * @throws TimeoutException if timeoutInSeconds is exceeded
+	 */
+	public WebElement find(WebDriver driver, String locator, int timeoutInSeconds) {
+		WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+		return wait.until(ExpectedConditions.presenceOfElementLocated(parse(locator)));
+	}
+
+	/**
+	 * Parses locator to an instance of {@link By}. Tries to emulate util IDE searching methods
+	 * <ul>
+	 * <li>By id: 'id=&lt;id&gt;</li>'
+	 * <li>By name: 'name=&lt;name&gt;</li>'
+	 * <li>By css selector: 'css=#&lt;id&gt;</li>'
+	 * <li>By link text selector: 'link=#&lt;linktext&gt;</li>'
+	 * <li>By xpath selector: 'div[@id=&lt;id&gt;]</li>'
+	 * </ul>
+	 *
+	 * @param locator to be parse to {@link By} instance
+	 * @return selector {@link By} instance selector
+	 */
+	private By parse(String locator) {
+		String cleanedLocator = this.fitnesseMarkup.clean(locator);
+		String selectorPrefix = StringUtils.substringBefore(cleanedLocator, SeleniumElementFinder.SELECTOR_TYPE_SEPARATOR);
+		SelectorType selectorType = EnumUtils.getEnum(SelectorType.class, selectorPrefix);
+		if(selectorType == null) {
+			return By.xpath(cleanedLocator);
+		}
+		String parsedLocator = StringUtils.removeStart(cleanedLocator, selectorPrefix + SeleniumElementFinder.SELECTOR_TYPE_SEPARATOR);
+		try {
+			return selectorType.byClass.getConstructor(String.class).newInstance(parsedLocator);
+		} catch (ReflectiveOperationException e) {
+			throw new IllegalStateException("Unexpected failure instantiating selector: " + selectorPrefix, e);
+		}
+	}
 }
