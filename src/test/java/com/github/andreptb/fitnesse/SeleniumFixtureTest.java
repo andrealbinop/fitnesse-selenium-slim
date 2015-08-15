d4abb1b874139340149107e7779d81cdc123065b
Andre Albino Pereira <andre@softplan.com.br>
2015-08-15 11:28:50 -0300

    prepping .travis.yml for 0.5.0 release, tests fully enabled

--- src/test/java/com/github/andreptb/fitnesse/SeleniumFixtureTest.java
+++ src/test/java/com/github/andreptb/fitnesse/SeleniumFixtureTest.java
@@ -9,7 +9,7 @@ import fitnesse.junit.FitNesseRunner;
  * Slim Fixture testing. Configured to run FitNesseSeleniumSlim.SeleniumFixtureTests suite.
  */
 @RunWith(FitNesseRunner.class)
-@FitNesseRunner.Suite(".FitNesseSeleniumSlim.SeleniumFixtureTests.SameBrowserSessionTests.TypeTests.ByXPathSelectorTest")
+@FitNesseRunner.Suite(".FitNesseSeleniumSlim.SeleniumFixtureTests.SameBrowserSessionTests")
 @FitNesseRunner.FitnesseDir("fitnesse")
 @FitNesseRunner.OutputDir("target/fitnesse")
 public class SeleniumFixtureTest {
